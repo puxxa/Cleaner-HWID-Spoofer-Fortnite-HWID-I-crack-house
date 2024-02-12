f365b0955009a1eab3f32d41314d107c89eaaa33
puxxa <118053695+puxxa@users.noreply.github.com>
2024-02-12 23:26:48 +0100

    Update Cleaner.bat

--- cleaner crack house/Cleaner.bat
+++ cleaner crack house/Cleaner.bat
@@ -1,13 +1,14 @@
 @echo off
-echo ░█████╗░██████╗░░█████╗░░█████╗░██╗░░██╗  ██╗░░██╗░█████╗░██╗░░░██╗░██████╗███████╗
-echo ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║░██╔╝  ██║░░██║██╔══██╗██║░░░██║██╔════╝██╔════╝
-echo ██║░░╚═╝██████╔╝███████║██║░░╚═╝█████═╝░  ███████║██║░░██║██║░░░██║╚█████╗░█████╗░░
-echo ██║░░██╗██╔══██╗██╔══██║██║░░██╗██╔═██╗░  ██╔══██║██║░░██║██║░░░██║░╚═══██╗██╔══╝░░
-echo ╚█████╔╝██║░░██║██║░░██║╚█████╔╝██║░╚██╗  ██║░░██║╚█████╔╝╚██████╔╝██████╔╝███████╗
-echo ░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝  ╚═╝░░╚═╝░╚════╝░░╚═════╝░╚═════╝░╚══════╝
-ping -n 6 127.0.0.1 > nul
-start cleancrackhouse1.bat
+echo POWERED BY CRACK HOUSE - PUXXA
+set /a "time=5"
+:loop
+echo clean start in %time%
 ping -n 2 127.0.0.1 > nul
-start cleancrackhouse2.bat
+set /a "time-=1"
+if %time% gtr 0 goto loop
+cd /d "%~dp0clean bat"
+start "" "cleancrackhouse1.bat"
 ping -n 2 127.0.0.1 > nul
-start cleancrackhouse3.bat
+start "" "cleancrackhouse2.bat"
+ping -n 2 127.0.0.1 > nul
+start "" "cleancrackhouse3.bat"
